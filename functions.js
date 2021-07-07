d0739e6083ea1af5192583c9ffcbfc1aa071934d
Cheri <mightypillow@tutanota.com>
2021-07-07 18:06:55 -0400

    Include base files with empty functions

---
+++ functions.js
@@ -0,0 +1,19 @@
+export const capitalize = function() {
+
+};
+
+export const reverseString = function(string) {
+
+};
+
+export const calculator = {
+
+};
+
+export const caesar = function() {
+
+};
+
+export const analyze = function() {
+
+};
