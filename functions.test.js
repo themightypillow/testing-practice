e732fecd30f608fbdb72f3bc59e8c6f2723223fa
Cheri <mightypillow@tutanota.com>
2021-07-07 18:37:06 -0400

    Write capitalize tests

---
+++ functions.test.js
@@ -0,0 +1,25 @@
+import { capitalize } from './functions';
+
+describe('capitalize', () => {
+  test('leave an empty string the same', () => {
+    expect(capitalize('')).toBe('');
+  });
+  test('capitalize a lowercase letter', () => {
+    expect(capitalize('a')).toBe('A');
+  });
+  test('leave an uppercase letter the same', () => {
+    expect(capitalize('B')).toBe('B');
+  });
+  test('leave a string with no letters the same', () => {
+    expect(capitalize('1234')).toBe('1234');
+  });
+  test('leave a capitalized string the same', () => {
+    expect(capitalize('Mighty')).toBe('Mighty');
+  });
+  test('capitalize an uncapitalized string', () => {
+    expect(capitalize('pillow')).toBe('Pillow');
+  });
+  test('throw an error for non-strings', () => {
+    expect(capitalize(1234)).toThrow('Not a string');
+  });
+});
